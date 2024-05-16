0c4988c537a6ec374c27cd72a2e9ce3a97b43f6e
neerajfullara <fullaraneeraj@gmail.com>
2024-05-16 11:42:55 +0530

    add some code for login-logout to make it more function with the sessions for user.

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template, g, request
+from flask import Flask, render_template, g, request, session, redirect, url_for
 from database_files import get_db
-
+import os
 # This library used to genrate the hash for the passwords and recheck for password hash for security
 from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.urandom(24)
 
 @app.teardown_appcontext
 def close_db(error):
@@ -13,7 +14,11 @@ def close_db(error):
 
 @app.route('/')
 def index():
-    return render_template('home.html')
+    user = None
+    if 'user' in session:
+        user = session['user']
+    
+    return render_template('home.html', user=user)
 
 @app.route('/register', methods=['GET','POST'])
 def register():
@@ -27,8 +32,23 @@ def register():
         return '<h1>user created!</h1>'
     return render_template('register.html')
 
-@app.route('/login')
+@app.route('/login', methods=['POST','GET'])
 def login():
+    if request.method == 'POST':
+        db = get_db()
+
+        name = request.form['name']
+        password = request.form['password']
+
+        user_cur = db.execute('select id, name, password from user where name = ?',[name])
+        user_result = user_cur.fetchone()
+
+        if check_password_hash(user_result['password'], password):
+            session['user'] = user_result['name']
+            return '<h1>pasword is correct</h1>'
+        else:
+            return '<h1>Password is incorect</h1>'
+
     return render_template('login.html')
 
 @app.route('/answer')
@@ -50,6 +70,11 @@ def unanswered():
 @app.route('/users')
 def users():
     return render_template('users.html')
+
+@app.route('/logout')
+def logout():
+    session.pop('user', None)
+    return redirect(url_for('index'))
 
 if __name__ == "__main__":
     app.run(debug=True)
