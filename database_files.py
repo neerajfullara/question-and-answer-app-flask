9493337b64ae413db802ec25bf9e24abc2a71f97
Neeraj Fullara <75372853+neerajfullara@users.noreply.github.com>
2024-12-03 16:03:22 +0530

    Update database_files.py
    deleted Unnecessary code.

--- database_files.py
+++ database_files.py
@@ -2,17 +2,7 @@ from flask import g
 import sqlite3
 import psycopg2
 from psycopg2.extras import DictCursor
-'''
-def connect_db():
-    sql = sqlite3.connect('C:/Users/fullara/Documents/Codes/Project/question-and-answer-app-flask/schemas.db')
-    sql.row_factory = sqlite3.Row
-    return sql
 
-def get_db():
-    if not hasattr(g, 'sqlite_db'):
-        g.sqlite_db = connect_db()
-    return g.sqlite_db
-'''
 def connect_db():
     conn = psycopg2.connect('postgresql://schemas_user:Uk1YqdEUMKtuN88bMzN0l6t5OpJufgaZ@dpg-cqcbjsg8fa8c73cmm5r0-a/schemas', cursor_factory=DictCursor)
     conn.autocommit = True
